9eac2e8f8235d317ecfdea41acfe6722b6b6cdde
Pavel-A-Nikolov <43548257+Pavel-A-Nikolov@users.noreply.github.com>
2022-02-18 18:26:44 +0200

    #5 task 1 - made an example repo;

---
+++ OMG.Classroom/DataAccessLayer/Repositories/RoleRepositories/IRoleRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.RoleRepositories
+{
+    public interface IRoleRepository
+    {
+        public void Add(Role role);
+        public Role Read(Guid id);
+        public IQueryable<Role> ReadAll();
+        public void Update(Role role);
+        public void Delete(Guid id);
+    }
+}
---
+++ OMG.Classroom/DataAccessLayer/Repositories/RoleRepositories/RoleRepository.cs
@@ -0,0 +1,67 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.RoleRepositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly ClassroomDbContext _dbContext;
+
+        public RoleRepository(ClassroomDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Add(Role role)
+        {
+            _dbContext.Roles.Add(role);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            Role role = _dbContext.Roles.Find(id);
+            if (role == null)
+            {
+                //Ако нещо подаде невалидно ид - малко се филмирах
+                throw new ArgumentNullException("No such role");
+            }
+            _dbContext.Roles.Remove(role);
+            _dbContext.SaveChanges();
+        }
+
+        public Role Read(Guid id)
+        {
+            Role role = _dbContext.Roles.Find(id);
+            if (role == null)
+            {
+                //Ако нещо подаде невалидно ид - малко се филмирах
+                throw new ArgumentNullException("No such role");
+            }
+            return role;
+        }
+
+        public IQueryable<Role> ReadAll()
+        {
+            return _dbContext.Roles.AsQueryable();
+        }
+
+        public void Update(Role newRole)
+        {
+            _dbContext.Roles.Update(newRole);
+
+            //Role role = _dbContext.Roles.Find(newRole.Id);
+            //if (role == null)
+            //{
+            //    //Ако нещо подаде невалидно ид - малко се филмирах
+            //    throw new ArgumentNullException("No such role");
+            //}
+            //_dbContext.Entry(role).CurrentValues.SetValues(newRole);
+            //_dbContext.SaveChanges();
+        }
+    }
+}
