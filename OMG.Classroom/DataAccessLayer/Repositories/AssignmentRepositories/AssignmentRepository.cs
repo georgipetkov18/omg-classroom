250681ca48ad43fdbb8a2756199d354f9b92b88f
Павел Николов <56231466+Pavel-Nikolov@users.noreply.github.com>
2022-02-21 23:27:04 +0200

    Repos (#3)
    * #5 task 1 - made an example repo;
    
    * #5 task 1 - what I do when s girl comes home
    
    * #5, task 1 - made BaseRepositories class and interface, also made methods async
    
    * #5 task 1 - implemented the nessesary changes
    
    * # 5 task 1 - now _dbSet is initialised in the perent constructor
    
    * #5 task 1 - made all repos
    
    * #5 task 1 - missing commas caused syntax errors
    
    Co-authored-by: Pavel-A-Nikolov <43548257+Pavel-A-Nikolov@users.noreply.github.com>

---
+++ OMG.Classroom/DataAccessLayer/Repositories/AssignmentRepositories/AssignmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.AssignmentRepositories
+{
+    public class AssignmentRepository : BaseRepository<Assignment>, IAssignmentRepository
+    {
+        public AssignmentRepository(ClassroomDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ OMG.Classroom/DataAccessLayer/Repositories/TeacherRepositories/TeacherRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.TeacherRepositories
+{
+    public class TeacherRepository : BaseRepository<Teacher>, ITeacherRepository
+    {
+        public TeacherRepository(ClassroomDbContext context) : base(context)
+        {
+        }
+    }
+}
