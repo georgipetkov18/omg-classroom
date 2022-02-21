c0bcc13d7208eee01f1968a5ac4eace874c32c5d
Павел Николов <56231466+Pavel-Nikolov@users.noreply.github.com>
2022-02-21 20:28:38 +0200

    Repos (#2)
    * #5 task 1 - made an example repo;
    
    * #5 task 1 - what I do when s girl comes home
    
    * #5, task 1 - made BaseRepositories class and interface, also made methods async
    
    * #5 task 1 - implemented the nessesary changes
    
    * # 5 task 1 - now _dbSet is initialised in the perent constructor
    
    Co-authored-by: Pavel-A-Nikolov <43548257+Pavel-A-Nikolov@users.noreply.github.com>

---
+++ OMG.Classroom/DataAccessLayer/Repositories/CourseRepositories/CourseRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.CourseRepositories
+{
+    public class CourseRepository : BaseRepository<Course>, ICourseRepository
+    {
+        public CourseRepository(ClassroomDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
