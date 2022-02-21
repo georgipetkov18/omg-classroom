af3a15152cbe4bf40ce3e935377ba21dc1d16bf0
Pavel-A-Nikolov <43548257+Pavel-A-Nikolov@users.noreply.github.com>
2022-02-21 23:15:11 +0200

    #5 task 1 - made all repos

---
+++ OMG.Classroom/DataAccessLayer/Repositories/MessageRepositories/MessageRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.MessageRepositories
+{
+    public class MessageRepository : BaseRepository<Message> IMessageRepository
+    {
+        public MessageRepository(ClassroomDbContext context) : base(context)
+        {
+        }
+    }
+}
