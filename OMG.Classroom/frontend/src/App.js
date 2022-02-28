4a4b799ec4ba2c3e4b1c39f9b15dcac433b3f90a
georgipetkov18 <george18petkov@gmail.com>
2022-03-01 00:10:39 +0200

    #7 - Task1: Functionality

--- OMG.Classroom/frontend/src/App.js
+++ OMG.Classroom/frontend/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RegisterPage from './pages/Register';
+import { useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const onUserAdded = (user) => {
+    setUsers(users => {
+      users.push(user);
+      return users;
+    })
+  };
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/register' element={<RegisterPage />}/>
-    </Routes>
+      <Routes>
+        <Route path='/register' element={<RegisterPage onSubmit={onUserAdded} />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- OMG.Classroom/frontend/src/pages/Register.js
+++ OMG.Classroom/frontend/src/pages/Register.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-export default function RegisterPage() {
+const RegisterPage = (props) => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const ageRef = useRef();
+    const passwordRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const user = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            age: ageRef.current.value,
+            password: passwordRef.current.value,
+        };
+        props.onSubmit(user);
+    };
+
     return (
         <div className='container register-form'>
             <h2 className='text-center'>Register</h2>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className='form-group'>
                     <label htmlFor='name'>Name</label>
-                    <input className='form-control' id='name' type="text"></input>
+                    <input className='form-control' id='name' type="text" ref={nameRef}></input>
                 </div>
 
                 <div className='form-group'>
                     <label htmlFor='email'>Email</label>
-                    <input className='form-control' id='email' type="email"></input>
+                    <input className='form-control' id='email' type="email" ref={emailRef}></input>
                 </div>
 
                 <div className='form-group'>
                     <label htmlFor='age'>Age</label>
-                    <input className='form-control' id='age' type="number"></input>
+                    <input className='form-control' id='age' type="number" ref={ageRef}></input>
                 </div>
 
                 <div className='form-group'>
                     <label htmlFor='password'>Password</label>
-                    <input className='form-control' id='password' type="text"></input>
+                    <input className='form-control' id='password' type="password" ref={passwordRef}></input>
                 </div>
 
                 <div className='d-flex justify-content-around btn-section'>
@@ -33,3 +49,5 @@ export default function RegisterPage() {
         </div>
     );
 }
+
+export default RegisterPage;
