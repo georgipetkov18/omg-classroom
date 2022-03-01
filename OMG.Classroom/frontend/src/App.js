6a14c6ac78df700d6982b84a30132bf1e0e5fae7
georgipetkov18 <george18petkov@gmail.com>
2022-03-01 11:32:26 +0200

    Follow folder naming conventions

--- OMG.Classroom/frontend/src/App.js
+++ OMG.Classroom/frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RegisterPage from './pages/Register';
+import RegisterPage from './components/pages/Register';
 import { useState } from 'react';
 
 function App() {
