8bc0a96f3ca26608d300c572415efc9b04622e9f
georgidomovchiyski03 <43469345+georgidomovchiyski03@users.noreply.github.com>
2022-03-24 15:30:51 +0200

    Made an example service (#13)
    * Made an example service
    
    * Remove Duplicate Repo folders
    
    * test
    
    * Sample services iteration1
    
    * services iteration1
    
    * Fixed typos and finilised services
    
    Co-authored-by: Georgi Domovchiyski <georgidomovchiyski@gmail.com>
    Co-authored-by: Pavel-A-Nikolov <43548257+Pavel-A-Nikolov@users.noreply.github.com>

---
+++ OMG.Classroom/ApplicationLayer/Services/ServiceInterfaces/IAssignmentService.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.Dtos.AssignmentDtos;
+
+namespace ApplicationLayer.Services
+{
+    public interface IAssignmentService
+    {
+        Task AddAsync(AssignmentDto assignmentDto);
+        Task DeleteAsync(Guid id);
+        Task<List<AssignmentDtoWithId>> ReadAllAsync();
+        Task<AssignmentDtoWithId> ReadAsync(Guid id);
+        Task UpdateAsync(AssignmentDtoWithId assignmentDto);
+    }
+}
