5941200cd6a8d49265b779e3f4afe6af15c1af46
Georgi Domovchiyski <georgidomovchiyski@gmail.com>
2022-03-30 14:48:41 +0300

    Added logic for attaching a student to course

--- OMG.Classroom/API/Controllers/StudentController.cs
+++ OMG.Classroom/API/Controllers/StudentController.cs
@@ -1,8 +1,10 @@
 ﻿using DataAccessLayer;
 using DataAccessLayer.Entities;
+using DataAccessLayer.Repositories.CourseRepositories;
 using DataAccessLayer.Repositories.StudentRepositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
@@ -13,9 +15,11 @@ namespace API.Controllers
     {
         //To be changed to service in the future and to DTOs/Models in the Future
         private readonly IStudentRepository _studentRepository;
-        public StudentController(IStudentRepository studentRepository)
+        private readonly ICourseRepository _courseRepository;
+        public StudentController(IStudentRepository studentRepository, ICourseRepository courseRepository)
         {
             _studentRepository = studentRepository;
+            _courseRepository = courseRepository;
         }
         [HttpGet]
         public async Task<IActionResult> GetAllAsync()
@@ -62,6 +66,27 @@ namespace API.Controllers
             //It will throw InternalServerError automatically upon exception
             await _studentRepository.UpdateAsync(student);
             return NoContent();
+        }
+        [Route("/attach")]
+        [HttpPost("{courseId}")]
+        public async Task<IActionResult> AttachStudentToCourse(Guid courseId, Student student)
+        {
+            //Logic to be move to service
+            var studentFromDB = await _studentRepository.ReadAsync(student.Id);
+            if (studentFromDB is null)
+            {
+                return NotFound();
+            }
+            var courseFromDB = await _courseRepository.ReadAsync(courseId);
+            if(courseFromDB is null)
+            {
+                return NotFound();
+            }
+            
+            studentFromDB.Courses.Add(courseFromDB);
+            await _studentRepository.UpdateAsync(student);
+            return NoContent();
+
         }
     }
 }
